38d7982b8b06c0424f628112c1ba5fc5d8652443
U-ALVA-DESKTOP\alvam <alvamao808@gmail.com>
2021-12-14 01:34:13 -0500

    fixed stability issue for k-means alg

--- k-means.py
+++ k-means.py
@@ -1,5 +1,6 @@
 import math
 import random
+import matplotlib.pyplot as plt
 
 #initializing variables
 numClusters=0   
@@ -8,6 +9,7 @@ maxShift=0
 maxLoop=0
 xVals = []
 yVals = []
+points = []
 counter = 0
 stabilized = True
 centroidX=[]
@@ -20,23 +22,25 @@ def main():
             row = line.split()
             xVals.append(int(row[0]))
             yVals.append(int(row[1]))
-    print("xValues: ",xVals)
-    print("yValues: ",yVals)
+            points.append((row[0], row[1]))
+    print("Points: ", points)
+    #print("xValues: ",xVals)
+    #print("yValues: ",yVals)
 
     #input number of clusters
-    numClusters=int(input("Enter in number of clusters: "))
+    numClusters=3#int(input("Enter in number of clusters: "))
     print("Number of Clusters:", numClusters, "\n")
 
     #input maximum shift threshold value
-    maxShift=float(input("Enter in maximum centroid shift threshold: "))
+    maxShift=.5#float(input("Enter in maximum centroid shift threshold: "))
     print("Maximum shift:", maxShift, "\n")
 
     #input threshold value
-    radius=int(input("Enter in centroid radius: "))
+    radius=10#int(input("Enter in centroid radius: "))
     print("Radius: ", radius, "\n")
 
     #input max loops
-    maxLoop=float(input("Enter in maximum amount of loops: "))
+    maxLoop=3#float(input("Enter in maximum amount of loops: "))
     print("Maximum loops:", maxLoop, "\n")
 
     #call generate_seed_points(points, num clusters)
@@ -44,15 +48,17 @@ def main():
 
     #setting count to 1 and stabilized to false, init outliers
     counter = 0
-    stabilized=False
+    stabilized=[0]*numClusters
     outliersX=[]
     outliersY=[]
-
+    outliersXY=[]
+    
     #big while loop that clusters
-    while counter < maxLoop and stabilized == False:
+    while counter < maxLoop and sum(stabilized) != numClusters:
         #these variables could do with being delcared before the while loop or before the function uses them to improve readability
         clusterX=[[] for _ in range(numClusters)] #declaring a list of lists with size numCluster
         clusterY=[[] for _ in range(numClusters)]
+        xyPoints=[[] for _ in range(numClusters)]
         pointArr=[]
         distArr = []
         tempDist = 0
@@ -78,24 +84,26 @@ def main():
                 #print("current point(x,y): ", xVals[i], yVals[i], "current centroid(x,y): ", centroidX[j], centroidY[j], "tempDist: ", tempDist)
             tp = pointdist(xVals[i], yVals[i], distArr)
             pointArr.append(tp)
-
+            
+        outliersXY.clear()
         outliersX.clear()
         outliersY.clear()
+
         #inserting into outliers and clusters
         for obj in pointArr:
-            #obj.printData()
             dist, i = obj.getIndex()
             if dist < radius:
                 clusterX[i].append(obj.getX())
                 clusterY[i].append(obj.getY())
+                xyPoints[i].append((obj.getX(), obj.getY()))
             else: 
                 outliersX.append(obj.getX())
                 outliersY.append(obj.getY())
+                outliersXY.append((obj.getX(), obj.getY()))
 
         #printing cluster values
         for i in range(numClusters):
-            print("Cluster: ", i+1, "x values: ", clusterX[i])
-            print("Cluster: ", i+1, "y values: ", clusterY[i])
+            print("Cluster: ", i+1, "xy values: ", xyPoints[i])
             if (len(clusterX[i])) == 0:
                 print("Centroid ", i+1, "has no points within radius. Exiting")
                 exit()
@@ -104,14 +112,13 @@ def main():
             #calculating x and y mean
             meanX = (sum(clusterX[i]))/(len(clusterX[i]))
             meanY = (sum(clusterY[i]))/(len(clusterY[i]))
-            print("Cluster: ", i+1, "Mean x: ", meanX, "Mean y: ", meanY)
+            print("Cluster: ", i+1, "Mean x y: (", meanX, meanY, ")")
 
             #calculating centroid shift
             tempDist = distCalc(meanX, meanY, centroidX[i], centroidY[i])
             if tempDist < maxShift:
-                stabilized = True
-                print("Centroid ", i+1 ,"shift : ", tempDist,"is less than max shift, stabilized. Exiting")
-                break
+                stabilized[i] = 1
+                print("Centroid ", i+1 ,"shift : ", tempDist,"is less than max shift, stabilized. ")
             else:
                 print("Centroid ", i+1 ,"shift: ", tempDist)
 
@@ -125,8 +132,7 @@ def main():
             print("Max Loop reached, exitng")
 
     #printing outlier values
-    print("Outliers x values: ", outliersX)
-    print("Outliers y values: ", outliersY)
+    print("Outliers xy values: ", outliersXY)
         
 
 # inputs set of points and number of clusters
